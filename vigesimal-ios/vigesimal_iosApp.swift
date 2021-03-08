a42f671a079a5db078877ecc646009b33efa3bf6
blendthink <info@blendthink.dev>
2021-03-08 09:47:27 +0900

    initial commit

---
+++ vigesimal-ios/vigesimal_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  vigesimal_iosApp.swift
+//  vigesimal-ios
+//
+//  Created by blendthink on 2021/03/08.
+//
+
+import SwiftUI
+
+@main
+struct vigesimal_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
